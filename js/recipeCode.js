b36c6cdd66eab60a8d71f6e80279c156e9088845
Sean Sweeney <59295068+Sean-Sweeney@users.noreply.github.com>
2023-11-09 15:04:52 -0500

    Nathan (#3)
    * Starting Recipe
    
    * basic project structure and skeleton home screen
    
    * suggested box
    
    * Update recipes page and remove unused assets
    
    * removed files
    
    * modifying home page, also initial shopping list page setup
    
    * Added bottom Nav, and recipe vertical stuff
    
    * add recipe
    
    * profile and upload
    
    * Added bottom nav bar
    
    * Finished navbar
    
    ---------
    
    Co-authored-by: Nathan <nathan19@umd.edu>
    Co-authored-by: Sean Sweeney <seansweeney@seans-mbp.mynetworksettings.com>
    Co-authored-by: Zian Wang <zwang127@terpmail.umd.edu>
    Co-authored-by: zlin1238 <123600587+zlin1238@users.noreply.github.com>

---
+++ js/recipeCode.js
@@ -0,0 +1,60 @@
+const activePage = window.location.pathname;
+const navLinks = document.querySelectorAll('.navigation ul li a').
+    forEach(link => {
+        console.log(activePage);
+        console.log(link.href);
+        if (link.href.includes(`${activePage}`)) {
+            link.classList.add("current");
+        }
+    });
+let popUp = document.getElementById("popup");
+
+
+function openPopup() {
+    popUp.classList.add("open-popup");
+}
+
+function closePopup() {
+    window.location.href = 'recipes.html'; // Change to your actual page URL
+}
+
+document.getElementById('recipeForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    console.log('Form submission prevented.');
+    // Create a new recipe object from the form inputs
+    let newRecipe = {
+      name: document.getElementById('recipeName').value,
+      prepTime: parseInt(document.getElementById('prepTime').value, 10),
+      cookTime: parseInt(document.getElementById('cookTime').value, 10),
+      ingredients: document.getElementById('ingredients').value.split(',')
+      // Add more fields as necessary
+    };
+
+    console.log('New recipe object:', newRecipe);
+  
+    // Retrieve the existing recipes from session storage
+    let recipes = JSON.parse(sessionStorage.getItem('recipes')) || [];
+  
+    // Add the new recipe to the array
+    recipes.push(newRecipe);
+  
+    // Save the updated recipes array back to session storage
+    sessionStorage.setItem('recipes', JSON.stringify(recipes));
+  
+    console.log('Recipes array after adding new recipe:', recipes);
+
+
+    // Confirming added recipe to add
+    openPopup();
+    /*
+    if (!anotherRecipe) {
+        // redirect to main recipe screen
+        window.location.href = 'recipes.html'; // Change to your actual page URL
+        console.log('Redirecting to recipes page.');
+    } else {
+        // Optionally clear the form or give the user feedback
+        document.getElementById('recipeForm').reset();
+    }
+    */
+  });
+
